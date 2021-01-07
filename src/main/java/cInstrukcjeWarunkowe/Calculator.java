e18f906385a692a7377d5053bcbc0f4ae1bd90fc
Zukoss <pawelzukowski22@gmail.com>
2021-01-07 10:48:55 +0100

    Commit Calculator 1

---
+++ src/main/java/cInstrukcjeWarunkowe/Calculator.java
@@ -0,0 +1,46 @@
+package cInstrukcjeWarunkowe;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Podaj pierwszą liczbę: ");
+        double firstNumber = sc.nextDouble();
+        System.out.println("Podaj drugą liczbę: ");
+        double secondNumber = sc.nextDouble();
+
+        System.out.println("Wybierz rodzaj operacji wpisując odpowiedni znak.");
+        System.out.println("Wykonanie dodawania wpisz => +");
+        System.out.println("Wykonanie odejmowania wpisz => -");
+        System.out.println("Wykonanie mnożenia wpisz => *");
+        System.out.println("Wykonanie dzielenia wpisz => /");
+
+        char sign = sc.next().charAt(0);
+
+            switch (sign) {
+                case '+':
+                    double x = firstNumber + secondNumber;
+                    System.out.println("Wynik dodawanie to: " + x);
+                    break;
+
+                case '-':
+                    double y = firstNumber - secondNumber;
+                    System.out.println("Wynik odejmowania to: " + y);
+                    break;
+
+                case '*':
+                    double z = firstNumber * secondNumber;
+                    System.out.println("Wynik mnożenia to: " + z);
+                    break;
+
+                case '/':
+                    double v = firstNumber / secondNumber;
+                    break;
+
+                default:
+                    System.out.println("Wpisz znak prawidłowy znak dla operacji podanych na początku programu.");
+            }
+    }
+}
