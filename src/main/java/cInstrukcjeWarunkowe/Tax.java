5448f326d2f1306b408a9b0c3fd1daf468472d22
Zukoss <pawelzukowski22@gmail.com>
2021-01-07 08:17:53 +0100

    Commit Tax 2

--- src/main/java/cInstrukcjeWarunkowe/Tax.java
+++ src/main/java/cInstrukcjeWarunkowe/Tax.java
@@ -10,12 +10,21 @@ public class Tax {
         double minTax = 0.17;
         double maxTax = 0.32;
 
-        System.out.println("Wprowadź wysokość przychodu:");
-        int myTax = sc.nextInt();
+        System.out.println("Wprowadź wysokość przychodu w złotówkach:");
+        int myIncome = sc.nextInt();
+        if (myIncome > 0) {
+            if (myIncome > taxLimit) {
+                double taxConstant = taxLimit * minTax;
+                double maxTaxLimit = ((myIncome - taxLimit) * maxTax);
+                double myTax = taxConstant + maxTaxLimit;
+                System.out.println("Twój podatek to: " + myTax + " zł.");
+            }
 
-        if (myTax > taxLimit) {
-            double x = ((myTax - taxLimit) * maxTax);
-            System.out.println("Twój podatek to: " + x);
+            if (myIncome < taxLimit) {
+                double myTax = myIncome * minTax;
+                System.out.println("Twój podatek to: " + myTax + " zł.");
+            }
         }
+        System.out.println("Wprowadź dodatnią sumę dochodu.");
     }
 }
