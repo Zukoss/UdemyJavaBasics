e81c87ea4c1c38637797759dd4447dec92e93244
Paul <pawelzukowski22@gmail.com>
2021-01-07 21:50:57 +0100

    Commit Loop

---
+++ src/main/java/dPetle/EvenNumb.java
@@ -0,0 +1,13 @@
+package dPetle;
+
+public class EvenNumb {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i <= 30; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Liczby parzyste: " + i);
+            }
+        }
+    }
+}
