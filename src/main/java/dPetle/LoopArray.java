a28401c1e8bbbb7c902facb582a99dc293a83e6a
Zukoss <pawelzukowski22@gmail.com>
2021-01-08 12:13:16 +0100

    Commit LoopArray.java

--- src/main/java/dPetle/LoopArray.java
+++ src/main/java/dPetle/LoopArray.java
@@ -2,6 +2,8 @@ package dPetle;
 
 // Utwórz tablicę rozmiaru 10 elementów i uzupełnij liczbami. Wypisz wszystkie liczby z tej tablicy na ekran. Następnie wypisz wszystkie liczby od tyłu.
 
+import java.util.Arrays;
+
 public class LoopArray {
     public static void main(String[] args) {
         int[] table = new int[10];
@@ -13,12 +15,23 @@ public class LoopArray {
             System.out.println("Wartość podnoszona do tablicy to: " +table[i]);
             count++;
         }
+
+        count--;
         System.out.println();
+
         for (int j = 0; j < table.length; j++) {
             System.out.println("Index - tablicy to: " + j);
             table[j] = count;
             System.out.println("Wartość podnoszona do tablicy to: " +table[j]);
             count--;
+        }
+
+        for (int k = 0; k < 10; k++) {
+            int suma = 0;
+            System.out.println("Sumowanie elementów tablicy:");
+            suma = suma + table[k];
+            System.out.println("Suma elementu ==> " + suma);
+            System.out.println(table[k]);
         }
     }
 }
--- src/main/java/dPetle/LoopExercise2.java
+++ src/main/java/dPetle/LoopExercise2.java
@@ -1,20 +1,1 @@
-package dPetle;
-
-// Podaj dwie dowolne liczby a i b, gdzie a jest mniejsze od b (a<b). Wypisz na ekran zakres liczb od a do b.
-
-public class LoopExercise2 {
-    public static void main(String[] args) {
-
-        int a = 45;
-        int b = 225;
-        int x = b - a;
-
-        for (int i = 0; i <= x; i++) {
-                System.out.println(a);
-                a++;
-                if (a >= b+1) {
-                    break;
-                }
-        }
-    }
-}
+package dPetle;// Podaj dwie dowolne liczby a i b, gdzie a jest mniejsze od b (a<b). Wypisz na ekran zakres liczb od a do b.public class LoopExercise2 {    public static void main(String[] args) {        int a = 45;        int b = 225;        int x = b - a;        for (int i = 0; i <= x; i++) {                System.out.println(a);                a++;                if (a >= b+1) {                    break;                }        }    }}
