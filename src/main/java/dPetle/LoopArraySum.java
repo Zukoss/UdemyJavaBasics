0a170814182d0bbc77edc70d8ed474143f94ca6a
Zukoss <pawelzukowski22@gmail.com>
2021-01-11 21:03:47 +0100

    Commit LoopArraySum.java fix

--- src/main/java/dPetle/LoopArraySum.java
+++ src/main/java/dPetle/LoopArraySum.java
@@ -3,12 +3,12 @@ package dPetle;
 public class LoopArraySum {
     public static void main(String[] args) {
 
-        int[] table = new int[10];
+        int[] table = new int[200];
         int suma = 0;
         int x = 0;
 
 
-        for (int i = 0; i <= 9; i++) {
+        for (int i = 0; i < table.length; i++) {
 
             x = table[i] = x;
             x++;
@@ -16,19 +16,5 @@ public class LoopArraySum {
             suma = suma + x;
             System.out.println("Suma tabeli: " + suma);
         }
-
-        System.out.println();
-        System.out.println("Wartość poza pętlą: " + x);
-        System.out.println();
-        System.out.println(table[0]);
-        System.out.println(table[1]);
-        System.out.println(table[2]);
-        System.out.println(table[3]);
-        System.out.println(table[4]);
-        System.out.println(table[5]);
-        System.out.println(table[6]);
-        System.out.println(table[7]);
-        System.out.println(table[8]);
-        System.out.println(table[9]);
     }
 }
