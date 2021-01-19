8029c52852c43a6011ad005c57afc21528c38bb5
Zukoss <pawelzukowski22@gmail.com>
2021-01-19 17:52:42 +0100

    Commit Class

---
+++ src/main/java/KlasyMetody/MethodTest.java
@@ -0,0 +1,10 @@
+package KlasyMetody;
+
+public class MethodTest {
+
+    int sumTest(int x, int y) {
+        int z = x + y;
+        System.out.println("Wynik dodawania: " + z);
+        return z;
+    }
+}
